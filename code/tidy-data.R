53eac40779370450ffc063fb27c08b6edef5319b
Tim-Markus <244563017+Tim-Markus@users.noreply.github.com>
2025-12-26 20:33:12 +0100

    work with polling data set

--- code/tidy-data.R
+++ code/tidy-data.R
@@ -711,3 +711,30 @@ gapminder %>% filter(region=="Caribbean") %>%
                           'Trinidad and Tobago' = "Trinidad")) %>%
   ggplot(aes(year, life_expectancy, color = country)) +
   geom_line()
+
+library(rvest)
+library(tidyverse)
+library(stringr)
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+tab <- read_html(url) %>% html_nodes("table")
+polls <- tab[[6]] %>% html_table(fill = TRUE)
+
+polls %>% head
+
+names(polls) <- c("dates", "remain", "leave", "undecided", "lead", "samplesize", "pollster", "poll_type", "notes")
+polls <- polls[str_detect(polls$remain, "%"), -9]
+nrow(polls)
+
+as.numeric(str_replace(polls$remain, "%", ""))/100
+
+polls %>% head
+
+polls$undecided <- str_replace(polls$undecided, "N/A", "0")
+polls
+
+polls$dates 
+
+temp <- str_extract_all(polls$dates, "\\d+\\s[a-zA-Z]+")
+end_date <- sapply(temp, function(x) x[length(x)])
+end_date
+
